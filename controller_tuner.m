7414614d3efb1834fd45d6426bdcd8bcd17d1480
LarsO <you@example.com>
2023-11-14 07:49:45 +0100

    Prepeard for controller tuning

--- controller_tuner.m
+++ controller_tuner.m
@@ -4,9 +4,7 @@ clc; clear; close all;
 
 
 % Initial gains:
-%gains_0 = [8, 0, 0.66667, 0, 0]; -> optimal_gains = [12.1176 0.0000 0.4409 -0.6268 0.3420];
-%gains_0 = [0, 0, 1, 0.25, 0.03]; % -> optimal_gains2 = [20.0000 0.6559 2.5116 0 0]
-gains_0 = [8 0 1.5 1 1];
+gains_0 = [16 0 750 1 1];
 
 
 %upperBound = [20, 20, 20, 1, 1];
@@ -20,22 +18,19 @@ beq = [];
 nonlcon = [];
 %options = optimoptions('fmincon','Display','iter','Algorithm','sqp');
 %  'interior-point'
-gains = fmincon(@costFun, gains_0, A, B, Aeq, beq, lowerBound, upperBound, nonlcon, options)
+%gains = fmincon(@costFun, gains_0, A, B, Aeq, beq, lowerBound, upperBound, nonlcon, options)
 
 
 % For testing
 %optimal_gains = [12.1176 0.0000 0.4409 -0.6268 0.3420]; % Gains from fmincon
 %optimal_gains2 = [20.0000 0.6559 2.5116 0 0]
 %my_gain = [8 0 1.5 1 1] % Manually tuned gains
-%error_RMS = costFun(my_gain)
+error_RMS = costFun(gains_0)
 
 
 
 function cost = costFun(gains)
 % Fixed parameters
-%zw = 1.5; % m
-%Tw = 10; % s
-%fw = 1/Tw; % Hz
 nsh = 2; % Number of sheaves
 ig = 11; % [-]
 mpl = 18000; % kg
@@ -53,16 +48,16 @@ zeta_valve = 1;
 deltap_valve_set = 15*1e5;
 
 % Non fixed parameters, corresponding to the choise of sytem
-Dm = 4.93; % motor dispalcemant
+Dm = 16;% % motor dispalcemant
 nm = 5; % Number of motors
 eta = 0.92;
 nmax_motor = 8000;
-inertia_motor = 0.0012;
+inertia_motor = 0.0004;
 Cd = 0.7;
 %Ad = (100/6e4)/(Cd*sqrt(2/875*(70*1e5)/2));
 %Ad = (150/6e4)/(Cd*sqrt(2/875*(70*1e5)/2));
 Ad = (200/6e4)/(Cd*sqrt(2/875*(70*1e5)/2));
-nv = 1; % Number of valves
+nv = 4; % Number of valves
 
 
 % Calculating gear ratio between wire and motor
